50e216443522e750b3b77dd7c48d0dc20ab86dce
Jonathan Convento <convento_nathan@yahoo.com>
2022-01-07 16:05:43 +0100

    initial Commit

---
+++ config/env/production/database.js
@@ -0,0 +1,29 @@
+const { parse } = require("pg-connection-string");
+
+modul.exports = ({ env }) => {
+    const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+    return {
+        defaultConnection: "default",
+        connections: {
+            default: {
+                connector: "bookshelf",
+                settings: {
+                    client: "postgres",
+                    host,
+                    port,
+                    database,
+                    username: user,
+                    password,
+                    ssl: { rejectUnauthorized: false }
+                    
+                },
+                options: {
+                    ssl: false
+                },
+            
+            },
+        },
+
+    };
+}
